ef6a39bc4852f4499542b06cda013ea95475efd1
Jacek Barc <jacekbarc@outlook.com>
2020-06-27 16:03:38 +0200

    Dodanie metody podłączenia do bazy danych

--- databaselibrary.cpp
+++ databaselibrary.cpp
@@ -4,3 +4,14 @@ DatabaseLibrary::DatabaseLibrary()
 {
 
 }
+
+bool DatabaseLibrary::cConnectDatabase(QString name, QString hostName, QString databaseName, QString userName, QString password)
+{
+    m_database = QSqlDatabase::addDatabase(name);
+    m_database.setHostName(hostName);
+    m_database.setDatabaseName(databaseName);
+    m_database.setUserName(userName);
+    m_database.setPassword(password);
+    return m_database.open();
+
+}
