bc3bd0077a9702b37a283acb89a5b791ea233ee4
Jacek Barc <jacekbarc@outlook.com>
2020-06-27 15:48:58 +0200

    Dodanie bibloteki bazy danych

---
+++ databaselibrary.cpp
@@ -0,0 +1,6 @@
+#include "databaselibrary.h"
+
+DatabaseLibrary::DatabaseLibrary()
+{
+
+}
---
+++ databaselibrary.h
@@ -0,0 +1,11 @@
+#ifndef DATABASELIBRARY_H
+#define DATABASELIBRARY_H
+
+
+class DatabaseLibrary
+{
+public:
+    DatabaseLibrary();
+};
+
+#endif // DATABASELIBRARY_H
