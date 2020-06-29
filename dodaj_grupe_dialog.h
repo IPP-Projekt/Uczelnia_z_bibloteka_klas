6bd2887c5560c047acc27329598f00a06fdf9367
Jacek Barc <jacekbarc@outlook.com>
2020-06-29 19:58:14 +0200

    Upadte dodawania grupy

--- dodaj_grupe_dialog.h
+++ dodaj_grupe_dialog.h
@@ -19,10 +19,13 @@ public:
     UczelniaDB* uczelnia;
     explicit Dodaj_grupe_Dialog(UczelniaDB* uczelniaPointer, QWidget *parent = nullptr);
     ~Dodaj_grupe_Dialog();
-    void setUczelnia(UczelniaDB* uczelniaPointer);
+    void setUczelnia(UczelniaDB* uczelniaPointer) {uczelnia = uczelniaPointer;}
 
 protected:
     void nowyrekord();
+
+private slots:
+    void on_buttonBox_accepted();
 
 private:
     Ui::Dodaj_grupe_Dialog *ui;
